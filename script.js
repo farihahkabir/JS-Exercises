88660cbe4ffc55da7d221934b493edec491da3a2
Farihah Kabir <nadahkabir@gmail.com>
2018-10-08 02:29:28 +0000

    JS third task

--- script.js
+++ script.js
@@ -10,4 +10,15 @@ function sumArray(arr){
     return sum;
 }
 
-console.log(sumArray([2,4,5,6,7]))
+console.log(sumArray([2,4,5,6,7]));
+console.log(sumArray([7,43,5,6,7]));
+
+//Task 3
+function checkEmail(emailString){
+    var emailFormat =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match = emailFormat.test(emailString);
+    return match;
+}
+
+console.log(checkEmail("nada@gmail.com"));
+console.log(checkEmail("vniaso.com"));
